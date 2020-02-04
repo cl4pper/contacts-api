a66ec03d32e9f33e5913e9a33c2d046b6dd7a5cd
cl4pper <cl4pper@gmail.com>
2020-02-03 23:53:00 -0300

    style(stock.js): apply linting for objects line breaks
    * delete unecessary files

--- src/routes/stock.js
+++ src/routes/stock.js
@@ -8,17 +8,23 @@ router.get('/api/stock', async (req, res) => {
 		const stock = await Stock.find();
 		res.send(stock);
 	} catch (err) {
-		res.status(500).json({ message: err });
+		res.status(500).json({
+			message: err
+		});
 	}
 });
 
 // GET ONE
 router.get('/api/stock/:id', async (req, res) => {
 	try {
-		const foundItem = await Stock.findOne({ _id: req.params.id });
+		const foundItem = await Stock.findOne({
+			_id: req.params.id
+		});
 		res.send(foundItem);
 	} catch (err) {
-		res.status(400).json({ message: err.message });
+		res.status(400).json({
+			message: err.message
+		});
 	}
 });
 
@@ -31,13 +37,17 @@ router.post('/api/stock', async (req, res) => {
 		const newItem = await item.save();
 		res.status(201).json(newItem);
 	} catch (err) {
-		res.status(400).json({ message: err.message });
+		res.status(400).json({
+			message: err.message
+		});
 	}
 });
 
 // DELETING ONE
 router.delete('/api/stock/:id', async (req, res) => {
-	const foundItem = await Stock.findOne({ _id: req.params.id });
+	const foundItem = await Stock.findOne({
+		_id: req.params.id
+	});
 
 	if (!foundItem) {
 		console.log('Item NOT found! -> to delete');
@@ -46,15 +56,21 @@ router.delete('/api/stock/:id', async (req, res) => {
 
 	try {
 		await foundItem.delete();
-		res.status(200).json({ message: 'Item deleted.' });
+		res.status(200).json({
+			message: 'Item deleted.'
+		});
 	} catch (err) {
-		res.status(400).json({ message: err.message });
+		res.status(400).json({
+			message: err.message
+		});
 	}
 });
 
 // UPDATING ONE
 router.patch('/api/stock/:id', async (req, res) => {
-	const foundItem = await Stock.findOne({ _id: req.params.id });
+	const foundItem = await Stock.findOne({
+		_id: req.params.id
+	});
 
 	if (!foundItem) {
 		console.log('Item NOT found -> to update');
@@ -62,10 +78,23 @@ router.patch('/api/stock/:id', async (req, res) => {
 	}
 
 	try {
-		await Stock.updateOne({ _id: req.params.id }, { $set: { ...req.body } });
-		res.status(200).json({ message: 'Item updated.' });
+		await Stock.updateOne(
+			{
+				_id: req.params.id
+			},
+			{
+				$set: {
+					...req.body
+				}
+			}
+		);
+		res.status(200).json({
+			message: 'Item updated.'
+		});
 	} catch (err) {
-		res.status(400).json({ message: err.message });
+		res.status(400).json({
+			message: err.message
+		});
 	}
 });
 
